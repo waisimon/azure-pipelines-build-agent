de77c48bb6c1e8356c30ad33b7fbb43b603a8b5b
Simon Chan <waisimon@outlook.com>
2021-09-11 00:54:10 +0800

    Update build-tools.sh

--- build-tools.sh
+++ build-tools.sh
@@ -10,13 +10,13 @@ fi
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 
 # Install Node & NPM
-sudo apt-get install gcc g++ make
-sudo apt-get install -y nodejs
+apt-get install gcc g++ make
+apt-get install -y nodejs
 
 # To install the Yarn package manager
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+apt-get update && sudo apt-get install yarn
 
 # Change ownership of the .npm directory to the sudo (non-root) user
 chown -R $SUDO_USER ~/.npm
