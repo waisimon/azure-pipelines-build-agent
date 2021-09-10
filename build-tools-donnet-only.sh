8906f1e332db5c4609f0921b56c54320cef843ff
Simon Chan <waisimon@outlook.com>
2021-09-11 01:02:16 +0800

    Create build-tools-donnet-only.sh

---
+++ build-tools-donnet-only.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Select a default .NET version if one is not specified
+if [ -z "$DOTNET_VERSION" ]; then
+  DOTNET_VERSION=5.0.203
+fi
+
+# Install .NET as the sudo (non-root) user
+sudo -i -u $
+_USER bash << EOF
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c Current -v $DOTNET_VERSION
+EOF
