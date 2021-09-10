11d9400608ee8a51aea836c4f670071ef40eee19
Simon Chan <waisimon@outlook.com>
2021-09-11 00:57:46 +0800

    Update build-tools.sh

--- build-tools.sh
+++ build-tools.sh
@@ -14,14 +14,15 @@ apt-get install gcc g++ make
 apt-get install -y nodejs
 
 # To install the Yarn package manager
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-apt-get update && sudo apt-get install yarn
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install yarn
 
 # Change ownership of the .npm directory to the sudo (non-root) user
 chown -R $SUDO_USER ~/.npm
 
 # Install .NET as the sudo (non-root) user
-sudo -i -u $SUDO_USER bash << EOF
+sudo -i -u $
+_USER bash << EOF
 curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -c Current -v $DOTNET_VERSION
 EOF
